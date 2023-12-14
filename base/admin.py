f2c4bc300df858c73b5ed4588a174a94cba58994
tuwafula <wafulaat@gmail.com>
2023-12-14 15:56:33 +0300

    fixed user issue

--- base/admin.py
+++ base/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Room, Topic, Message, User 
+from .models import Room, Topic, Message
+from django.contrib.auth import get_user_model
+
+User = get_user_model() 
 
 admin.site.register((Room, Topic, Message, User))
