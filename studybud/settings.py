ec8542510b99e35fee463d1b57e9e42bb2d89b62
tuwafula <wafulaat@gmail.com>
2023-12-14 14:58:50 +0300

    added pillow

--- studybud/settings.py
+++ studybud/settings.py
@@ -136,8 +136,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
